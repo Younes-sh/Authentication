e49c614974b4045cbd9135f74130e3b7d7d0f7be
younes-sh <sheikhlaryounes@gmail.com>
2025-01-01 17:45:18 +0100

    i added Navbar in _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+// Importing Bootstrap JS
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.js';
+import Navbar from "@/Components/Navbar/Navbar";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
