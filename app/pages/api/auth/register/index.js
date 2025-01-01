86617f0fa6e31d43deee3605b4f529d3a8c1b188
younes-sh <sheikhlaryounes@gmail.com>
2025-01-01 19:32:33 +0100

    I edited pages/api/auth/register/index.js

--- app/pages/api/auth/register/index.js
+++ app/pages/api/auth/register/index.js
@@ -7,10 +7,10 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     } else {
-        const { firstname, lastname, username, email, password, city, state, zip, imageProfile } = req.body;
+        const { firstname, lastname, username, email, password, city, state, zip } = req.body;
         try {
             const hashedPassword = await bcrypt.hash(password, 10);
-            await User.create({ firstname, lastname, username, email, password: hashedPassword, city, state, zip, imageProfile });
+            await User.create({ firstname, lastname, username, email, password: hashedPassword, city, state, zip });
             return res.status(201).json({ message: 'User created successfully' });
         } catch (error) {
             return res.status(500).json(error);
